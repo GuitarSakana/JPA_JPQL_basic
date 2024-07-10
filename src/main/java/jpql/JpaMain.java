1513980fb0ad6700d47dd146063fa0076e6dd294
Guitarfish <ayrcap@gmail.com>
2024-07-10 11:54:00 +0900

    Jpa Main test

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -25,13 +25,34 @@ public class JpaMain {
             for (Member member1 : resultList) {
                 System.out.println(member1);
             }*/
+
+            /*
             List<Member> test = em.createQuery("select m from Member m where m.username =: username", Member.class)
                     .setParameter("username","홍길동")
                     .getResultList();
 
             for (Member member1 : test) {
                 System.out.println(member1.getUsername());
+            }*/
+
+            /*List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+            for (Member findMember : result) {
+                System.out.println(findMember.getUsername());
+                System.out.println(findMember.getAge());
+            }/*/
+
+            /*List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class).getResultList();
+            if(result.isEmpty()) System.out.println("없어 이뇨석아!");*/
+
+            /*List<Adress> result2 = em.createQuery("select o.adress from Order o", Adress.class).getResultList();*/
+
+            List<MemberDto> result = em.createQuery("select new jpql.MemberDto(m.username, m.age) from Member m ", MemberDto.class).getResultList();
+            for (MemberDto findMemberDto : result) {
+                System.out.println(findMemberDto.getUsername());
+                System.out.println(findMemberDto.getAge());
             }
+
+
 
             tx.commit();
         }catch (Exception e){
